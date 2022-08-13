c177788f82c9e88c8bdcee04760d5b27b33ca262
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2022-08-13 12:55:24 +0300

    Adds exmaple of class

---
+++ Person.ps1
@@ -0,0 +1,32 @@
+Set-StrictMode -Version 3
+
+class Person {
+    static [int]$MIN_AGE = 18
+
+    [ValidateNotNullOrEmpty()][string]$name
+    [int]$yob
+
+    Person([string]$name, [int]$yob) {
+        # Write-Output doesn't work because only the return values are sent 
+        # to the pipeline. To display anything we need to use Write-Host
+        Write-Host "Type of `$yob = $($yob.GetType().Name)"
+        $this.name = $name
+        $this.yob = $yob
+    }
+
+    [int] GetAge() {
+        return 2022 - $this.yob
+    }
+
+}
+
+$p1 = [Person]::new("Mihai", 1990)
+
+Write-Output $p1.GetAge()
+
+$minAge = [Person]::MIN_AGE
+
+Write-Output "`$minAge = $minAge"
+Write-Output $([Person]::MIN_AGE)
+
+# [Person]::new("", 1990)
